2628fcb446ea7e62e3ea8d8c8fdfeb80d3498581
KelvinJoelC <Kelvinjcarreno@gmail.com>
2018-04-06 14:30:28 +0200

    Funcion comprobar

--- nuevo_3_en_raya/main.cpp
+++ nuevo_3_en_raya/main.cpp
@@ -9,29 +9,45 @@ const char simbolo[J] = { 'X', 'O' };
 void limpiar(char tablero[N][N]){
 
     for (int f=0; f<N; f++)
-	for (int c=0; c<N; c++)
-	    tablero[f][c] = ' ';
+        for (int c=0; c<N; c++)
+            tablero[f][c] = ' ';
 }
 
 void imprimir(char tablero[N][N]){
     system("clear");
     for(int f=0; f<N; f++){
-	for (int c=0; c<N; c++)
-	    printf(" %c %c", tablero[f][c], c !=2 ? '|' : ' ');
-	if (f !=2)
-	    printf("\n---+---+---\n");
-    } 
+        for (int c=0; c<N; c++)
+            printf(" %c %c", tablero[f][c], c !=2 ? '|' : ' ');
+        if (f !=2)
+            printf("\n---+---+---\n");
+    }
     printf("\n");
 }
 
 void preguntar(int turno, int *f,int  *c){
 
     printf("Jugador %i: \n", turno % 2 + 1);
-    printf("Introduce las coordenadas: fila,columna ");
+    printf("Introduce las coordenadas: fila,columna (0,0) ");
     scanf(" %i , %i", f, c);
 
 }
 
+void comprobar(int turno, char tablero[N][N]){
+    for (int a=0; a<N; a++){
+        if (tablero[a][0] == tablero[a][1] && tablero[a][1]== tablero[a][2])
+            printf("Jugador %i, has ganado!!",turno % 2 + 1);
+        if (tablero[0][a] == tablero[1][a] && tablero[1][a]== tablero[2][a])
+            printf("Jugador %i, has ganado!!",turno % 2 + 1);
+        }
+        if (tablero[0][0] == tablero[1][1] == tablero[2][2])
+            printf("Jugador %i, has ganado!!",turno % 2 + 1);
+
+        if (tablero[0][1] == tablero[1][1] == tablero[2][1])
+            printf("Jugador %i, has ganado!!", turno % 2 +1);
+        if (tablero[1][0] == tablero[1][1] == tablero[1][2])
+            printf("Jugador %i, has ganado!!", turno % 2 +1);
+
+    }
 int main(){
 
     int turno = 0;
@@ -39,11 +55,17 @@ int main(){
     char tablero[N][N];
 
     limpiar(tablero);
-    for (turno=0; turno<9; turno++){
-	imprimir(tablero);
-	preguntar(turno, &f, &c);
-	tablero[f][c] = simbolo[turno % 2];
+    for (turno=0; turno<10; turno++){
+        imprimir(tablero);
+        preguntar(turno, &f, &c);
+        if  (tablero[f][c] == ' ')
+            tablero[f][c] = simbolo[turno % 2];
+        else
+            turno--;
+
+       comprobar(turno, tablero);
     }
+
 
     return EXIT_SUCCESS;
 }
