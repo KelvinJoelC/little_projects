ab5ce1fba3682810188ba55c193177d252668689
KelvinJoelC <Kelvinjcarreno@gmail.com>
2018-04-12 12:54:16 +0200

    Programa finalizado

--- Tres_en_raya/Tres_en_raya.cpp
+++ Tres_en_raya/Tres_en_raya.cpp
@@ -2,91 +2,94 @@
 
 #include <stdio.h>
 #include <stdlib.h>
- 
-#define N 3
-#define J 2
 
-const char simbolo[J] = { 'X', 'O' };
+#define N 3
+
+
+const char simbolo[] = { 'X', 'O' };
 bool finish = false;
 
 void limpiar(char tablero[N][N]){
 
-	for (int f=0; f<N; f++)
-		for (int c=0; c<N; c++)
-			tablero[f][c] = ' ';
+    for (int f=0; f<N; f++)
+        for (int c=0; c<N; c++)
+            tablero[f][c] = ' ';
 }
 
 void imprimir(char tablero[N][N]){
-	system("clear");
-	for(int f=0; f<N; f++){
-		for (int c=0; c<N; c++)
-			printf(" %c %c", tablero[f][c], c !=2 ? '|' : ' ');
-		if (f !=2)
-			printf("\n---+---+---\n");
-	}
-	printf("\n");
+    system("clear");
+    for(int f=0; f<N; f++){
+        for (int c=0; c<N; c++)
+            printf(" %c %c", tablero[f][c], c !=2 ? '|' : ' ');
+        if (f !=2)
+            printf("\n---+---+---\n");
+    }
+    printf("\n");
 }
 
 void preguntar(int turno, int *f,int  *c){
-	
-	printf("Jugador %i: \n", turno % 2 + 1);
-	printf("Introduce las coordenadas: fila,columna x,y del 0-2 ");
-	scanf(" %i , %i", f, c);
+
+    printf("Jugador %i: \n", turno % 2 + 1);
+    printf("Introduce las coordenadas: fila,columna x,y del 0-2 (%c): ",simbolo[turno % 2]);
+    scanf(" %i , %i", f, c);
 
 }
 void ganador(int turno, char tablero[N][N]){
-	system("clear");
-	imprimir(tablero);
-	printf("\n");
-	printf("Jugador %i, has ganado!!\n",turno % 2 + 1);
-        finish = true;
+    system("clear");
+    imprimir(tablero);
+    printf("\n");
+    printf("Jugador %i, has ganado!!\n",turno % 2 + 1);
+    finish = true;
 }
 
 void comprobar(int turno, char tablero[N][N]){
 
-	if (tablero[0][0] != ' '){
-		if (tablero[0][0] == tablero[0][1] && tablero[0][0] == tablero[0][2])
-			ganador(turno,tablero);
-		if (tablero[0][0] == tablero[1][0] && tablero[1][0]== tablero[2][0])
-			ganador(turno,tablero);
-		if (tablero[0][0] == tablero[1][1] && tablero[1][1]== tablero[2][2])
-			ganador(turno,tablero);
-	}
-	if (tablero[0][1] != ' ')
-		if (tablero[0][1] == tablero[1][1] && tablero[0][1]== tablero[2][1])
-			ganador(turno,tablero);
+    if (tablero[0][0] != ' '){
+        if (tablero[0][0] == tablero[0][1] && tablero[0][0] == tablero[0][2])
+            ganador(turno,tablero);
+        if (tablero[0][0] == tablero[1][0] && tablero[1][0]== tablero[2][0])
+            ganador(turno,tablero);
+        if (tablero[0][0] == tablero[1][1] && tablero[1][1]== tablero[2][2])
+            ganador(turno,tablero);
+    }
+    if (tablero[0][1] != ' ')
+        if (tablero[0][1] == tablero[1][1] && tablero[0][1]== tablero[2][1])
+            ganador(turno,tablero);
 
-	if (tablero[1][2] != ' '){
-		if (tablero[1][2] == tablero[0][2] && tablero[1][2] == tablero[2][2])
-			ganador(turno,tablero);
-		if (tablero[1][2] == tablero[1][1] && tablero[1][2]== tablero[1][0])
-			ganador(turno,tablero);
-	}
-	if (tablero[2][1] != ' ')
-		if (tablero[2][2] == tablero[2][1] && tablero[2][2] == tablero[2][0])
-			ganador(turno,tablero);
+    if (tablero[1][2] != ' '){
+        if (tablero[1][2] == tablero[0][2] && tablero[1][2] == tablero[2][2])
+            ganador(turno,tablero);
+        if (tablero[1][2] == tablero[1][1] && tablero[1][2]== tablero[1][0])
+            ganador(turno,tablero);
+    }
+    if (tablero[2][1] != ' ')
+        if (tablero[2][2] == tablero[2][1] && tablero[2][2] == tablero[2][0])
+        ganador(turno,tablero);
+    if (tablero[0][2] != ' ')
+        if (tablero[0][2] == tablero[1][1] && tablero[0][2] == tablero[2][0])
+        ganador(turno,tablero);
 } 
 
 int main(){
 
-	int turno = 0;
-	int f,c;
-	char tablero[N][N];
+    int turno = 0;
+    int f,c;
+    char tablero[N][N];
 
-	limpiar(tablero);
-	do {	
-	imprimir(tablero);
-		preguntar(turno, &f, &c);
-		if  (tablero[f][c] == ' '){
-			tablero[f][c] = simbolo[turno % 2];
-			imprimir(tablero);}
-		else
-			turno--;
-		turno++;
-		comprobar(turno, tablero);
-		
-	}while (turno < 9 && !finish);
-	printf("Empate!! Ninguno gana!!\n");
+    limpiar(tablero);
+    do {	
+        imprimir(tablero);
+        preguntar(turno, &f, &c);
+        if  (tablero[f][c] == ' '){
+            tablero[f][c] = simbolo[turno % 2];
+            imprimir(tablero);}
+        else
+            turno--;
+        turno++;
+        comprobar(turno, tablero);
 
-	return EXIT_SUCCESS;
+    }while (turno < 9 && !finish);
+    if (turno>=9 && finish==false)
+        printf("Empate!! Ninguno gana!!\n");
+    return EXIT_SUCCESS;
 }
