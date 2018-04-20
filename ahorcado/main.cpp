d760468417a9469687dff1c74d14444217e3f9d1
KelvinJoelC <Kelvinjcarreno@gmail.com>
2018-04-20 14:29:04 +0200

    :D

--- ahorcado/main.cpp
+++ ahorcado/main.cpp
@@ -1,13 +1,32 @@
 #include <stdlib.h>
 #include <stdio.h>
+
+#include <string.h>
 #define N 0x100
+
+
+void inicio(int n_letras, int contador_vida){
+    system("toilet -fpagga AHORCADO");
+  for(int i=0; i < n_letras; i++)
+  printf("_ ");
+  for(int s=0; s < contador_vida; s++);
+  printf(" %s", contador_vida =  2 ? "♡ ": "♥ ");
+}
+
+void ganas(){}
+void pierdes(){}
+void comprueba_letra(){
+
+}
+
 int main(){
-    char letras[N][N];
+    
+    int contador_vida = 6;
+    char letras[N]{'j','i','r','a','f','a'};
+    int tamanopalabra = strlen(letras);
+    inicio(tamanopalabra, contador_vida);
     int letra;
-    scanf(" %c", &letra);
-    malloc
-
-
+    printf("%i",tamanopalabra);
 
     return 0;
 
